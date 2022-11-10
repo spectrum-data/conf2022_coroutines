6df350f8d4ec8e1131f4ab64a792ee807a2b519d
comdiv <fagim.sadykov@gmail.com>
2022-11-10 13:09:31 +0500

    comdiv/p06s01

--- src/main/kotlin/impl/CoroutinesP06S01.kt
+++ src/main/kotlin/impl/CoroutinesP06S01.kt
@@ -15,6 +15,14 @@ object CoroutinesP06S01 {
         onException: (message: String) -> Unit = {},
         body: suspend () -> Unit
     ): Job {
-        TODO("Not yet implemented")
+        return launch {
+            try{
+                body()
+            }catch (e: CancellationException) {
+                onCancel(e.message ?: "")
+            }catch (e: Throwable){
+                onException(e.message ?: "")
+            }
+        }
     }
 }
