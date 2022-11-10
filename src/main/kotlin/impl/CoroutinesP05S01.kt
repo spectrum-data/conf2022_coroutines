d7c79dda9efc971354b0fff068119f1c979f4e04
comdiv <fagim.sadykov@gmail.com>
2022-11-10 12:42:40 +0500

    comdiv/p05s01

--- src/main/kotlin/impl/CoroutinesP05S01.kt
+++ src/main/kotlin/impl/CoroutinesP05S01.kt
@@ -2,6 +2,7 @@ package impl
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
 import kotlinx.coroutines.selects.select
 
 /**
@@ -10,13 +11,28 @@ import kotlinx.coroutines.selects.select
  * У вас есть два канала с сообщениями.
  * Требуется вернуть новый канал для логирования и отправлять в него сообщения из обоих каналов при их получении.
  * Логировать сообщения нужно до тех пор, пока один из входящих каналов не будет закрыт.
- * Подсказка: используйте выражение `select` для выполнения этого задания. 
+ * Подсказка: используйте выражение `select` для выполнения этого задания.
  */
 object CoroutinesP05S01 {
     fun CoroutineScope.logMessages(
         messageChannel1: ReceiveChannel<String>,
         messageChannel2: ReceiveChannel<String>
     ): ReceiveChannel<String> {
-        TODO("Not yet implemented")
+        return produce {
+            while (!messageChannel1.isClosedForReceive && !messageChannel2.isClosedForReceive) {
+                select {
+                    messageChannel1.onReceiveCatching { m ->
+                        if(m.isSuccess) {
+                            send(m.getOrThrow())
+                        }
+                    }
+                    messageChannel2.onReceiveCatching { m ->
+                        if(m.isSuccess) {
+                            send(m.getOrThrow())
+                        }
+                    }
+                }
+            }
+        }
     }
 }
